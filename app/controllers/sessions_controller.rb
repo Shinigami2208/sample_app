d6d14d1f386b04e69845a2d320200563541a3c87
Bui Huu Hoang <bhhoang1998@gmail.com>
2020-09-09 23:51:01 +0700

    chapter 12: Reset password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by email: params[:session][:email].downcase
     if user.try :authenticate, params[:session][:password]
-      if user.activated?
-        log_in user
-        params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-        redirect_back_or user
-      else
-        flash[:warning] = t "global.account_not_activated"
-        redirect_to root_url
-      end
+      user_activated user
     else
       flash.now[:danger] = t "global.flash_login_danger"
       render :new
@@ -27,5 +20,16 @@ class SessionsController < ApplicationController
 
   def checkbox_remember user
     params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+  end
+
+  def user_activated user
+    if user.activated?
+      log_in user
+      checkbox_remember user
+      redirect_back_or user
+    else
+      flash[:warning] = t "global.account_not_activated"
+      redirect_to root_url
+    end
   end
 end
